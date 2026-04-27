08dbf645617360dba4cc0b54c57370f2faf6453a
Akansha Roy <akansharoy2008@gmail.com>
2026-04-28 00:24:06 +0530

    implemented sse logs

--- backend/src/routes/deploymentRoutes.ts
+++ backend/src/routes/deploymentRoutes.ts
@@ -1,5 +1,11 @@
 import { Router } from "express";
 import {
+  addClient,
+  removeClient,
+  getLogs,
+  addLog,
+} from "../services/logService";
+import {
   createDeployment,
   getDeployments,
 } from "../services/deploymentService";
@@ -15,12 +21,39 @@ router.post("/", (req, res) => {
 
   const id = createDeployment(repoUrl);
 
+  setTimeout(() => addLog(id, "Cloning repo..."), 1000);
+  setTimeout(() => addLog(id, "Installing dependencies..."), 2000);
+  setTimeout(() => addLog(id, "Building project..."), 3000);
+  setTimeout(() => addLog(id, "Deployment complete!"), 4000);
+
   res.json({ id });
 });
 
 router.get("/", (req, res) => {
   const deployments = getDeployments();
   res.json(deployments);
+});
+
+//SSE logs
+router.get("/:id/logs", (req, res) => {
+  const { id } = req.params;
+
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Connection", "keep-alive");
+
+  res.flushHeaders();
+
+  const existingLogs = getLogs(id);
+  existingLogs.forEach((log) => {
+    res.write(`data: ${log}\n\n`);
+  });
+
+  addClient(id, res);
+
+  req.on("close", () => {
+    removeClient(id, res);
+  });
 });
 
 export default router;
---
+++ backend/src/services/logService.ts
@@ -0,0 +1,36 @@
+const logs: Record<string, string[]> = {};
+const clients: Record<string, any[]> = {};
+
+export function addLog(deploymentId: string, message: string) {
+  if (!logs[deploymentId]) {
+    logs[deploymentId] = [];
+  }
+
+  logs[deploymentId].push(message);
+
+  if (clients[deploymentId]) {
+    clients[deploymentId].forEach((res) => {
+      res.write(`data: ${message}\n\n`);
+    });
+  }
+}
+
+export function getLogs(deploymentId: string) {
+  return logs[deploymentId] || [];
+}
+
+export function addClient(deploymentId: string, res: any) {
+  if (!clients[deploymentId]) {
+    clients[deploymentId] = [];
+  }
+
+  clients[deploymentId].push(res);
+}
+
+export function removeClient(deploymentId: string, res: any) {
+  if (!clients[deploymentId]) return;
+
+  clients[deploymentId] = clients[deploymentId].filter(
+    (client) => client !== res
+  );
+}
